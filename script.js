361148bbec863fdd125c357fc9f2bedd2db7f3b6
Sakeena Marikkar <twinklesakeena@gmail.com>
2024-12-28 17:22:47 +0530

    6thcommit

--- script.js
+++ script.js
@@ -67,3 +67,14 @@ document.addEventListener('DOMContentLoaded', function() {
         navBar.classList.toggle('hidden');
     });
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
